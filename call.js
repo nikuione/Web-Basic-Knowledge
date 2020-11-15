4346a1883cc68fe1635626913adea6e507f28e6c
nikuione <nikuione@users.noreply.github.com>
2020-11-15 19:08:13 +0800

    Update call.js

--- call.js
+++ call.js
@@ -1,35 +1,8 @@
-// 第一版
-Function.prototype.call2 = function(context) {
-    // 首先要获取调用call的函数，用this可以获取
-    context.fn = this;
-    context.fn();
-    delete context.fn;
-}
-
-// 测试一下
-var foo = {
-    value: 1
-};
-
-function bar() {
-    console.log(this.value);
-}
-
-bar.call2(foo); // 1
-
-
-
-// 第三版
-Function.prototype.call2 = function (context) {
+Function.prototype.call2 = function (context, ...args) {
     var context = context || window;
     context.fn = this;
 
-    var args = [];
-    for(var i = 1, len = arguments.length; i < len; i++) {
-        args.push('arguments[' + i + ']');
-    }
-
-    var result = eval('context.fn(' + args +')');
+    var result = context.fn(...args);
 
     delete context.fn
     return result;
